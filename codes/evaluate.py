b264ccf96225a8dfbb3866fe835a973c547c6cc9
acetheberliner <tommaso.bagnolini@studio.unibo.it>
2024-09-09 19:13:22 +0200

    modifica delle funzionalità come su richiesta

--- codes/evaluate.py
+++ codes/evaluate.py
@@ -1,7 +1,6 @@
-
 import torch
 
-def evaluate(model, val_loader, criterion):
+def evaluate(model, val_loader, criterion, return_preds=False):
     """
     Valuta il modello sul dataset di validazione.
 
@@ -9,43 +8,41 @@ def evaluate(model, val_loader, criterion):
         model (torch.nn.Module): modello da valutare
         val_loader (torch.utils.data.DataLoader): dataloader del dataset di validazione
         criterion (torch.nn.Module): funzione di perdita da utilizzare
+        return_preds (bool): se True, restituisce anche le etichette e le predizioni
 
     Returns:
         val_loss (float): perdita media sul dataset di validazione
         val_accuracy (float): accuratezza del modello sul dataset di validazione
+        val_labels (list, opzionale): etichette vere se return_preds è True
+        val_preds (list, opzionale): predizioni del modello se return_preds è True
     """
     # Imposta il modello in modalità valutazione
     model.eval()
 
-    # Inizializza variabili per tracciare la perdita totale, predizioni corrette e campioni totali
     total_loss = 0.0
     total_correct = 0
     total_samples = 0
+    val_labels = []
+    val_preds = []
     
     # Disabilita il calcolo del gradiente per migliorare le prestazioni
     with torch.no_grad():
-        # Itera sul dataset di validazione
         for inputs, labels in val_loader:
-            # Esegui un passaggio in avanti e ottieni le uscite del modello
             outputs = model(inputs)
-            
-            # Calcola la perdita tra le uscite e le etichette vere
             loss = criterion(outputs, labels)
-            
-            # Accumula la perdita totale
             total_loss += loss.item()
-            
-            # Ottieni le etichette di classe predette
             _, predicted = torch.max(outputs, 1)
-            
-            # Aggiorna il numero totale di campioni e predizioni corrette
             total_samples += labels.size(0)
             total_correct += (predicted == labels).sum().item()
             
-    # Calcola la perdita media e l'accuratezza
+            if return_preds:
+                val_labels.extend(labels.cpu().numpy())
+                val_preds.extend(predicted.cpu().numpy())
+    
     val_loss = total_loss / len(val_loader)
     val_accuracy = 100 * total_correct / total_samples
     
-    # Restituisci la perdita media e l'accuratezza
-    return val_loss, val_accuracy
-
+    if return_preds:
+        return val_loss, val_accuracy, val_labels, val_preds
+    else:
+        return val_loss, val_accuracy
